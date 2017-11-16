a629d549913e77a8c585f0c30d14d57bd4c90c62
emersonlbr <emersonlbr@icloud.com>
2017-11-15 23:51:08 -0200

    variables

---
+++ jsbasics/script.js
@@ -0,0 +1,12 @@
+// before using frameworks you must know javascript
+// HMTL + CSS + JS = Sucess!
+
+console.log("fuck you emerson");
+
+// variables
+var name = prompt("whats your name?");
+var lastName = prompt("whats your last name?");
+var age = prompt("whats your age?");
+
+console.log(name + ' ' + lastName + ' is ' + age + ' years old');
+
