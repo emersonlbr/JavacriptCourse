7c6309614bb2d8aad00f4712469897e7f7378f18
emersonlbr <emersonlbr@icloud.com>
2017-11-19 10:30:52 -0200

    else/if statements

--- jsbasics/script.js
+++ jsbasics/script.js
@@ -3,10 +3,49 @@
 
 console.log("fuck you emerson");
 
-// variables
-var name = prompt("whats your name?");
+// ** variables
+
+/*var name = prompt("whats your name?");
 var lastName = prompt("whats your last name?");
 var age = prompt("whats your age?");
 
 console.log(name + ' ' + lastName + ' is ' + age + ' years old');
+*/
+
+
+
+// ** Operators
+/* var now = 2016
+var bday = now - 26;
+bday = now - 26 * 2;
+
+console.log(bday);
+*/
+
+
+
+// ** if/else statemaents
+
+/* var name = 'Emerson';
+var age = 26;
+var isMarried = "yes";
+
+if (isMarried === 'yes') {
+  console.log(name + 'is married');
+} else {
+  console.log(name + ' will hopefuly marry soon');
+}
+
+// using a boolean value
+isMarried = false;
+
+if (isMarried) {
+  console.log('YES');
+} else {
+  console.log('NO');
+}
+*/
+
+
+
 
